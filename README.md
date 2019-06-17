c31a88604f8ed2d7345896c96badd8b12205c134
Steven D Vachon <contact@svachon.com>
2019-06-17 18:22:55 -0400

    v2.0.0

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# is-dom-orphan
-Determine whether a node exists within the DOM tree.
+# is-dom-detached [![NPM Version][npm-image]][npm-url] ![File Size][filesize-image] [![Build Status][travis-image]][travis-url] [![Coverage Status][coveralls-image]][coveralls-url] [![Dependency Monitor][greenkeeper-image]][greenkeeper-url]
+
+> Determine if a [`Node`](https://mdn.io/Node) does *not* exist within a DOM tree.
+
+
+## Installation
+
+[Node.js](http://nodejs.org) `>= 10` is required. To install, type this at the command line:
+```shell
+npm install is-dom-detached
+```
+
+
+## Importing
+
+ES Module:
+```js
+import isDetachedNode from 'is-dom-detached';
+```
+
+CommonJS Module:
+```js
+const isDetachedNode = require('is-dom-detached');
+```
+
+
+## Usage
+
+```js
+const div = document.createElement('div');
+isDetachedNode(div); //-> true
+
+document.body.appendChild(div);
+isDetachedNode(div); //-> false
+````
+
+
+[npm-image]: https://img.shields.io/npm/v/is-dom-detached.svg
+[npm-url]: https://npmjs.com/package/is-dom-detached
+[filesize-image]: https://img.shields.io/badge/size-490B%20gzipped-blue.svg
+[travis-image]: https://img.shields.io/travis/stevenvachon/is-dom-detached.svg
+[travis-url]: https://travis-ci.org/stevenvachon/is-dom-detached
+[coveralls-image]: https://img.shields.io/coveralls/stevenvachon/is-dom-detached.svg
+[coveralls-url]: https://coveralls.io/github/stevenvachon/is-dom-detached
+[greenkeeper-image]: https://badges.greenkeeper.io/stevenvachon/is-dom-detached.svg
+[greenkeeper-url]: https://greenkeeper.io/
---
+++ test.js
@@ -0,0 +1,283 @@
+"use strict";
+const {after, before, it} = require("mocha");
+const puppeteer = require("puppeteer");
+const puppeteerCoverage = require("puppeteer-to-istanbul");
+
+const runInBrowser = func => () => page.evaluate(func);
+
+let browser, page;
+
+
+
+// @todo also use npmjs.com/puppeteer-firefox
+before(async () =>
+{
+	browser = await puppeteer.launch({ args: ["--no-sandbox"] });
+	page = await browser.newPage();
+
+	page.on("console", async msg => console[msg._type](...await Promise.all(msg.args().map(arg => arg.jsonValue()))));
+	page.on("pageerror", console.error);
+
+	await Promise.all(
+	[
+		page.addScriptTag({ path: "node_modules/chai/chai.js" }),
+		page.addScriptTag({ path: "temp.js" }),
+
+		// @todo https://github.com/istanbuljs/puppeteer-to-istanbul/issues/18
+		// @todo https://github.com/GoogleChrome/puppeteer/issues/3570
+		page.coverage.startJSCoverage({ reportAnonymousScripts: true })
+	]);
+
+	await page.evaluate(() =>
+	{
+		const iframe = document.createElement("iframe");
+		document.body.append(iframe);
+		window.anotherRealm = iframe.contentWindow;
+		window.anotherDocument = anotherRealm.document;
+
+		window.expect = chai.expect;
+		delete window.chai; // cleanup
+	});
+});
+
+
+
+after(async () =>
+{
+	let coverage = await page.coverage.stopJSCoverage();
+
+	// Exclude tools
+	coverage = coverage.filter(({url}) => !url.includes("chai"));
+
+	puppeteerCoverage.write(coverage);
+
+	browser.close();
+});
+
+
+
+it("is a (bundled) function", runInBrowser(() =>
+{
+	expect(window.isDetachedNode).to.be.a("function");
+}));
+
+
+
+it("returns false for a non-Node", runInBrowser(() =>
+{
+	const fixtures =
+	[
+		"Node",
+		Symbol("Node"),
+		{},
+		[],
+		/regex/,
+		true,
+		1,
+		null,
+		undefined,
+		window,
+		anotherRealm
+	];
+
+	fixtures.forEach(fixture => expect(isDetachedNode(fixture)).to.be.false);
+}));
+
+
+
+it("supports same- and cross-Realm Nodes", runInBrowser(() =>
+{
+	const tests =
+	[
+		// Same Realm
+		{
+			node: document.createComment("data"),
+			target: document.body
+		},
+		{
+			node: document.createElement("tagName"),
+			target: document.body
+		},
+		{
+			node: document.createProcessingInstruction("target", "data"),
+			target: document.body
+		},
+		{
+			node: document.createTextNode("data"),
+			target: document.body
+		},
+		{
+			node: anotherDocument.createComment("data"),
+			target: anotherDocument.body
+		},
+		{
+			node: anotherDocument.createElement("tagName"),
+			target: anotherDocument.body
+		},
+		{
+			node: anotherDocument.createProcessingInstruction("target", "data"),
+			target: anotherDocument.body
+		},
+		{
+			node: anotherDocument.createTextNode("data"),
+			target: anotherDocument.body
+		},
+
+		// Mixed Realms
+		{
+			node: document.createComment("data"),
+			target: anotherDocument.body
+		},
+		{
+			node: document.createElement("tagName"),
+			target: anotherDocument.body
+		},
+		{
+			node: document.createProcessingInstruction("target", "data"),
+			target: anotherDocument.body
+		},
+		{
+			node: document.createTextNode("data"),
+			target: anotherDocument.body
+		},
+		{
+			node: anotherDocument.createComment("data"),
+			target: document.body
+		},
+		{
+			node: anotherDocument.createElement("tagName"),
+			target: document.body
+		},
+		{
+			node: anotherDocument.createProcessingInstruction("target", "data"),
+			target: document.body
+		},
+		{
+			node: anotherDocument.createTextNode("data"),
+			target: document.body
+		}
+	];
+
+	tests.forEach(({node, target}) =>
+	{
+		expect(isDetachedNode(node)).to.be.true;
+
+		target.append(node);
+
+		expect(isDetachedNode(node)).to.be.false;
+
+		node.remove();
+	});
+}));
+
+
+
+it("supports same- and cross-Realm Document Nodes", runInBrowser(() =>
+{
+	const tests =
+	[
+		{
+			node: document,
+			result: false
+		},
+		{
+			node: document.createDocumentFragment(),
+			result: true,
+		},
+		{
+			node: document.implementation.createDocument("namespaceURI", "qualifiedNameStr"),
+			result: true
+		},
+		{
+			node: document.implementation.createHTMLDocument("title"),
+			result: true
+		},
+		{
+			node: anotherDocument,
+			result: false
+		},
+		{
+			node: anotherDocument.createDocumentFragment(),
+			result: true,
+		},
+		{
+			node: anotherDocument.implementation.createDocument("namespaceURI", "qualifiedNameStr"),
+			result: true
+		},
+		{
+			node: anotherDocument.implementation.createHTMLDocument("title"),
+			result: true
+		}
+	];
+
+	tests.forEach(({node, result}) => expect(isDetachedNode(node)).to.equal(result));
+}));
+
+
+
+it("supports seldom-used Nodes", runInBrowser(() =>
+{
+	const nodes =
+	[
+		document.implementation.createDocument("namespaceURI", "qualifiedNameStr").createCDATASection("data"),
+		document.implementation.createDocumentType("qualifiedNameStr", "publicId", "systemId")
+	];
+
+	nodes.forEach(node => expect(isDetachedNode(node)).to.be.true);
+}));
+
+
+
+it("supports Nodes inside a DocumentFragment", runInBrowser(() =>
+{
+	const fragment = document.createDocumentFragment();
+	const parent = document.createElement("div");
+	const target = document.createTextNode("content");
+
+	expect(isDetachedNode(target)).to.be.true;
+
+	parent.append(target);
+	fragment.append(parent);
+
+	expect(isDetachedNode(target)).to.be.true;
+
+	document.body.append(fragment);
+
+	expect(isDetachedNode(target)).to.be.false;
+
+	parent.remove();
+}));
+
+
+
+it("supports Nodes inside <body>", runInBrowser(() =>
+{
+	const parent = document.createElement("div");
+	const target = document.createTextNode("content");
+
+	expect(isDetachedNode(target)).to.be.true;
+
+	parent.append(target);
+	document.body.append(parent);
+
+	expect(isDetachedNode(target)).to.be.false;
+
+	parent.remove();
+}));
+
+
+
+it("supports Nodes outside <body>", runInBrowser(() =>
+{
+	const parent = document.createElement("div");
+	const target = document.createTextNode("content");
+
+	expect(isDetachedNode(target)).to.be.true;
+
+	parent.append(target);
+	document.head.append(parent);
+
+	expect(isDetachedNode(target)).to.be.false;
+
+	parent.remove();
+}));
